26b9c9c18c6f8016ae56d5a20521f9e630cd7cfd
RomaRoma <v12nes11@gmail.com>
2021-08-24 23:40:45 +0300

    JS-done

--- js/script.js
+++ js/script.js
@@ -1,40 +1,48 @@
 'use strict';
+import {MDCSlider} from '@material/slider';
 
+const slider = new MDCSlider(document.querySelector('.mdc-slider'));
 let wrapper = document.querySelector('.inputs'),
     firstElement = document.querySelector('.inputs').firstElementChild,
     firstElementValue,
     secondElement = document.querySelector('.inputs').lastElementChild,
     secondElementValue,
     button = document.querySelector('.getResult'),
-    addOne = document.querySelector('.addOne'),
-    removeOne = document.querySelector('.removeOne'),
     result = document.querySelector('.result');
 
-
-
 button.addEventListener('click', () => {
-    firstElementValue = Number(firstElement.value);
-    secondElementValue = Number(secondElement.value);
-    const operator = document.querySelector('input[name=selector]:checked').value;
-    result.innerHTML = (sum(firstElementValue, secondElementValue));
-    console.log(operator);
 
-    function sum(a, b) {
-        if (operator === '+') {
-            return a + b;
-        } else if (operator === '-') {
-            return a - b;
-        } else if (operator === '*') {
-            return a * b;
-        } else if (operator === '/') {
-            return a / b;
-        }
+
+
+    if (firstElement.value !== '' && secondElement.value !== '') {
+
+        const firstElementValue = Number(firstElement.value),
+            secondElementValue = Number(secondElement.value),
+            operator = document.querySelector('input[name=selector]:checked'),
+            reg = new RegExp('^[0-9]+$');
+
+        let count = function (a, b) {
+            if (operator.value === '+') {
+                return a + b;
+            } else if (operator.value === '-') {
+                return a - b;
+            } else if (operator.value === '*') {
+                return a * b;
+            } else if (operator.value === '/') {
+                return a / b;
+            }
+        };
+
+        result.innerHTML = (count(firstElementValue, secondElementValue));
+
+        const resetButton = document.createElement('button');
+        resetButton.classList.add('resetButton');
+        resetButton.innerHTML = 'Reset';
+        document.body.append(resetButton);
+        
+        console.error(resetButton);
+        
+    } else {
+        result.innerHTML = ('Please fill all the fields');
     }
 });
-
-addOne.addEventListener('click', () => {
-    firstElement.value++;
-})
-removeOne.addEventListener('click', () => {
-    firstElement.value--;
-})
